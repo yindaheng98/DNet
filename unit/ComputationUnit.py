1cf3fc7238565da7722c18591e93edbb884aa133
yindaheng98 <yindaheng98@163.com>
2020-10-18 20:58:48 +0800

    计算层抽象类完成·

--- unit/ComputationUnit.py
+++ unit/ComputationUnit.py
@@ -6,20 +6,26 @@
 """
 
 import torch
+from ComputationQueue import ComputationQueue, ComputationRequest
 
 
-class ComputationUnit:
+class ComputationUnit(object):
     """
     分支网络组成单元中的计算层
     """
-    def __init__(self, net, thres):
+
+    def __init__(self, net, thres, computation_queue):
         """
         初始化函数
-        @param net: 要使用的网络，类参考MP_inception_cifar10.py文件
+        @param net: 用于计算的神经网络，类参考MP_inception_cifar10.py文件
         @param thres: 一个数组，每个退出点的退出阈值
+        @param commication_unit: 用于接收请求进行的类，必须继承自CommunicationUnit类
         """
         self.net = net
         self.thres = thres
+        if not isinstance(computation_queue, ComputationQueue):
+            raise TypeError("computation_queue必须是ComputationQueue的子类")
+        self.q = computation_queue
 
     def compute(self, x, start_layer, exit_layer):
         """
@@ -32,7 +38,7 @@ class ComputationUnit:
 
         # 执行推断
         x, outputs = self.net(x, exit_layer, start_layer)
-        # TODO: 如果需要更加通用的计算层，最好自定义一个抽象的网络单元结构（就像torch.nn.Module那样）
+        # TODO: 如果需要更加通用的计算层，最好研究出一个抽象的DDNN网络计算单元（就像torch.nn.Module那样）
 
         # 取得分最大的类别，所为当前任务推断结果
         prob, category = torch.max(outputs.data, 1)
@@ -42,3 +48,16 @@ class ComputationUnit:
             return False, (x, exit_layer + 1)
         # 否则返回识别结果
         return True, category
+
+    def start(self):
+        """
+        启动一个死循环从self.q队列中读取数据、处理、返回结果
+        顺序进行
+        TODO: 实际情况下肯定需要考虑各种超时出错失败退出的情况
+        """
+        while True:
+            req = self.q.pop()  # 接收一个数据
+            if not isinstance(req, ComputationRequest):
+                raise TypeError("ComputationQueue.pop的返回值必须是ComputationRequest的子类")
+            ok, result = self.compute(req.x, req.start_layer, req.exit_layer)
+            req.put_result(ok, result)
